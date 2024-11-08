3bd261e2d8ce83416279d98bb05a7f9a4a0e0814
Panca-Satia <pancasatianugraha@gmail.com>
2024-11-08 13:49:31 +0700

    2.AngkaRomawipart2.py

--- 2.AngkaRomawipart2.py
+++ 2.AngkaRomawipart2.py
@@ -12,13 +12,9 @@ romawi = int(input("\nMasukkan bilangan bulat untuk menentukan angka romawi: "))
 angka_romawi = ""
 nilai_romawi = [
     (1000, "M"),
-    (900, "CM"),
-    (500, "D"),
     (400, "CD"),
     (100, "C"),
-    (90, "XC"),
     (50, "L"),
-    (40, "XL"),
     (10, "X"),
     (9, "IX"),
     (5, "V"),
